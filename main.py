b81ac88276c069d02a5ca6a098567f69dadeb3d4
South2190 <south02274653@gmail.com>
2021-10-16 03:20:30 +0900

    ソース更新
    v1.0.0に更新

--- main.py
+++ main.py
@@ -6,19 +6,14 @@ import yaml
 import os
 from datetime import datetime
 from logging import getLogger, config
-#import concurrent.futures
+
 # トークンデータの読み込み
 import OAuthData
-
-# 変数の宣言
-global oauth
-channel_id = OAuthData.discord_channel
-account_id = OAuthData.twitter_account_id
-focus_account = OAuthData.twitter_name
 
 # 前処理
 client = discord.Client()
 
+# ロガーの準備
 LOG = getLogger(__name__)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 print(os.getcwd())
@@ -27,13 +22,15 @@ config.dictConfig(yaml.load(open('log_config.yaml').read(), Loader=yaml.SafeLoad
 # 起動時に動作する処理
 @client.event
 async def on_ready():
-	# 起動したらチャンネルにログイン通知が送信される
-	login_channel = client.get_channel(channel_id)
-	s_msg = await login_channel.send('起動しました')
+	# 起動したらチャンネルにログイン通知を送信
+	login_channel = client.get_channel(OAuthData.discord_channel)
+	#s_msg = await login_channel.send('起動しました')
 	LOG.info('ログインしました')
+
 	# 10秒待った後に起動通知メッセージを削除
 	os.system('timeout 11')
-	await s_msg.delete()
+	#await s_msg.delete()
+
 	# Twitterからツイートを取得
 	oauth = twitter.OAuth(OAuthData.access_token, OAuthData.access_token_secret, OAuthData.consumer_key, OAuthData.consumer_secret)
 	twitter_api = twitter.Twitter(auth=oauth)
@@ -44,7 +41,7 @@ async def on_ready():
 		try:
 			# 特定ユーザーのツイートをリアルタイムで受信
 			LOG.info('ツイートの受信を開始します')
-			for tweet in stream.statuses.filter(follow = account_id):
+			for tweet in stream.statuses.filter(follow = OAuthData.twitter_account_id):
 				# ツイートを出力
 				tl = '({time} tweetID:{id}) [@{username}]:{text}\n'.format(time=datetime.now().strftime("%Y/%m/%d %H:%M:%S"), id = tweet['id'], username = tweet['user']['screen_name'], text = tweet['text'])
 				print(tl)
@@ -57,19 +54,21 @@ async def on_ready():
 				print('LUNATIC' in tweet['text'])
 				print('追加' in tweet['text'] and ('新曲' in tweet['text'] or '楽曲' in tweet['text']))
 				print(any([('追加' in tweet['text'] and ('新曲' in tweet['text'] or '楽曲' in tweet['text'])), '一挙公開' in tweet['text'], 'LUNATIC' in tweet['text']]))
-				print(tweet['user']['screen_name'] == focus_account)
+				print(tweet['user']['screen_name'] == OAuthData.twitter_name)
 				"""
 
 				# ツイートに特定のワードが含まれていた場合
-				if any([('追加' in tweet['text'] and ('新曲' in tweet['text'] or '楽曲' in tweet['text'])), '一挙公開' in tweet['text'], 'LUNATIC' in tweet['text']]) and tweet['user']['screen_name'] == focus_account:
+				if any([('追加' in tweet['text'] and ('新曲' in tweet['text'] or '楽曲' in tweet['text'])), '一挙公開' in tweet['text'], 'LUNATIC' in tweet['text']]) and tweet['user']['screen_name'] == OAuthData.twitter_name:
 					LOG.debug(tl)
 					LOG.info("ツイートが見つかりました")
 					url = 'https://twitter.com/{user}/status/{tweetid}'.format(user = tweet['user']['screen_name'], tweetid = tweet['id'])
 					await login_channel.send(url)
 
+				"""
 				# ツイートに「exit」のワードが含まれていた場合
 				if 'exit' in tweet['text']:
 					exit()
+				"""
 
 		# 原因不明のKeyErrorはログを吐かせて無視する
 		except KeyError as ke:
