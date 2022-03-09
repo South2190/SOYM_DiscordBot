7ad91c1e52c333fc3ad5c6222eb838c88ec6ce0b
South2190 <south02274653@gmail.com>
2022-03-10 01:25:50 +0900

    v2.0.0.220309
    v2.0.0.220309

---
+++ BotSettings.py
@@ -0,0 +1,24 @@
+"""
+TwitterBot紐づけアカウント:@hoge_id
+"""
+consumer_key			= ''
+consumer_secret			= ''
+access_token			= ''
+access_token_secret		= ''
+
+"""
+Discord設定
+"""
+# ウェブフックURL
+webhook_uri				= ''
+
+# 送信時に使用するBotの名前
+discord_bot_name		= 'オンゲキ新曲通知Bot(v2.0.0)'
+
+# アイコン画像URL
+icon_uri				= ''
+
+"""
+ツイート取得対象アカウント:@ongeki_official
+"""
+twitter_account_id		= 923845298027294720
